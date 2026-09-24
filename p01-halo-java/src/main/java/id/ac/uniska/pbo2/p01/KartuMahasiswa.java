6c8ff5b4443c057bb7727b25827efe132a8f3e3b
Nama Lengkap Anda <2410010337bayu@gmail.com>
2026-09-24 14:14:46 +0800

    P01: proyek pertama HaloPBO2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class KartuMahasiswa {
+        public static void main (String []args){
+            String nama = "Muhammmad Bayu Saputra";
+            String NPM = "2410010337";
+            String Prodi = "Teknik Informatika";
+            int Semester = 5;
+            String Alasan = "Menghasilkan suatu aplikasi";
+            
+             System.out.println("==========================================");
+             System.out.println(" KARTU MAHASISWA PBO 2");
+             System.out.println("==========================================");
+             System.out.println(" Nama      : " + nama );
+             System.out.println(" NPM       : " + NPM );
+             System.out.println(" Prodi     : " + Prodi );
+             System.out.println(" Semester  : " + Semester );    
+             System.out.println(" Alasan    : " + Alasan );    
+             System.out.println("=========================================");    
+        }
+}
